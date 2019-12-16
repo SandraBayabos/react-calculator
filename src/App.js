53052cd33c7bea056f3b36a7b00beb9c53350bfc
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-12-16 15:46:27 +0800

    created interface for calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import KeyPad from "./components/KeyPad";
+import CalculatorDisplay from "./components/CalculatorDisplay";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ width: "100%" }}>
+        <div
+          style={{
+            width: "350px",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CalculatorDisplay />
+          <KeyPad />
+        </div>
+      </div>
+    </>
   );
 }
 
